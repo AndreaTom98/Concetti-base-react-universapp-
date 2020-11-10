fa7d26f0d5b505723d931bc698b53fe09f4ef6ee
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-11-10 13:51:28 +0100

    state on change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import {useState} from 'react';
 
 function App() {
-  const [numero, setNumero] = useState(0)
-  const aumentaNumero = () => {
-    setNumero(numero / 2)
+  const [text, setText] = useState("")
+  const onChangeHandler = (e) => {
+    setText(e.target.value)
   }
   return (
     <div className="App">
-      <button onClick={aumentaNumero}>aumenta numero</button>
-      <h3>ciao {numero}</h3>
+     <input type="text" onChange={onChangeHandler} value={text} />
+     {text}
+     <button onClick={() => setText("")}>azzera state</button>
     </div>
   );
 }
