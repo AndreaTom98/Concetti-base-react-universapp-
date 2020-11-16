a2dd39563ed22d96d2c9698a44915a3a9a02f5fe
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-11-16 11:58:11 +0100

    lavorare coon state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import {useState} from 'react';
+import VideoText from './VideoText';
 
 function App() {
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
+  const [textList, setTextList] = useState([])
   const onChangeHandler = (e) => {
     setText(e.target.value)
   }
+  const addTextHandler = () => {
+    setTextList([...textList, text]);
+    // 1. ...textList => copia l'array attuale
+    // 2. text => aggiunge text a textList
+
+    setText("")
+  }
+
   return (
     <div className="App">
      <input type="text" onChange={onChangeHandler} value={text} />
-     {text}
-     <button onClick={() => setText("")}>azzera state</button>
+     <button onClick={addTextHandler}>aggiungi text</button>
+     <ul>
+     {textList.map(function(text) {
+       return <li>{text}</li>
+     })}
+     </ul>
     </div>
   );
 }
