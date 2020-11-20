047c2458462b4dfff2552616710aa5ce790022a1
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-11-20 11:04:41 +0100

    sass

--- src/VideoText.js
+++ src/VideoText.js
@@ -1,10 +1,9 @@
-import style from './style/VideoText.module.css'
-import styled from 'styled-components'
-
+import style from './style/VideoText.module.css';
+import './style/myStyle.scss'
 
 function VideoText({title, color, deleteVideo}) {
     return (
-        <div style={{display: 'flex'}}>
+        <div id="container" style={{display: 'flex'}}>
             <div style={{padding: '30px', background: color}}>
                 <h1>image</h1>
                 <button onClick={deleteVideo}>cancella video</button>
